c8cb1972bb1a1915ba982e141976073ed93a9d81
Сергей <sbukij@gmail.com>
2022-06-01 17:40:22 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
 
+ 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// Сoздаем ключ для хранилища
+const STORAGE_KEY = "videoplayer-current-time"; 
+
+const onPlay = function ({ seconds }) {
+    // делает новую запись в хранилище
+    localStorage.setItem(STORAGE_KEY, seconds);
+};
+// console.log(onPlay);
+player.on('timeupdate', throttle(onPlay, 1000)); 
+const time = localStorage.getItem(STORAGE_KEY);
+if (time) {
+    player.setCurrentTime(time);
+}
+// возвращает из хранилища значение с ключом STORAGE_KEY
+
+  
